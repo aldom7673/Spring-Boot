5193e9ffde2ccab901060f87a60e7cc287f2f40e
= <aldom7673@gmail.com>
2020-04-06 18:25:14 -0500

    Inicio configuracion Spring

---
+++ demoartifact/src/test/java/comgroupid/demoartifact/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package comgroupid.demoartifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
