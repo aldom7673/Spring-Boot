524fe31e9f69410beed083e0f1a53ed55e4501e2
= <aldom7673@gmail.com>
2020-04-07 14:16:02 -0500

    Implementacion de metodos de modelos

---
+++ demoartifact/src/main/java/comgroupid/demoartifact/Service/DependenciaServiceImpl.java
@@ -0,0 +1,36 @@
+package comgroupid.demoartifact.Service;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import comgroupid.demoartifact.Modelo.Modelo;
+
+@Service
+public class DependenciaServiceImpl implements IDependenciaService{
+    private List<Modelo> lista = null;
+
+    public DependenciaServiceImpl(){
+        lista = new LinkedList<Modelo>();
+        lista.add(new Modelo(1,"Aldo"));
+        lista.add(new Modelo(2,"Daniel"));
+        lista.add(new Modelo(3,"Mendoza"));
+        lista.add(new Modelo(4,"Morales"));
+    }
+
+    @Override
+    public List<Modelo> obtenerModelos() {
+        return lista;
+    }
+
+    @Override
+    public Modelo buscarPorId(int idModelo) {
+        for(Modelo m : lista){
+            if(m.getId() == idModelo){
+                return m;
+            }
+        }
+        return null;
+    }
+}
