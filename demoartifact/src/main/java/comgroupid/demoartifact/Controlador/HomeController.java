91de3b0e2d650cc1459f837d695f3c2e2ff20f15
= <aldom7673@gmail.com>
2020-04-07 14:15:47 -0500

    Lista de modelo

--- demoartifact/src/main/java/comgroupid/demoartifact/Controlador/HomeController.java
+++ demoartifact/src/main/java/comgroupid/demoartifact/Controlador/HomeController.java
@@ -3,14 +3,20 @@ package comgroupid.demoartifact.Controlador;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import comgroupid.demoartifact.Modelo.Modelo;
+import comgroupid.demoartifact.Service.IDependenciaService;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private IDependenciaService serviceDependencia;
 
     @GetMapping("/")
     public String Inicio(Model model){
@@ -27,6 +33,30 @@ public class HomeController {
 
         Modelo modelo = new Modelo(1, "Aldo");
         model.addAttribute("modelo", modelo);
+        return "home";
+    }
+
+    @GetMapping("/listaAutowired")
+    public String InicioAutowired(Model model){
+        model.addAttribute("mensaje","Mensaje desde model");
+        
+        Modelo modelo = new Modelo();
+        model.addAttribute("modelo", modelo);
+
+        List<Modelo> lista = serviceDependencia.obtenerModelos();
+        model.addAttribute("modelos", lista);
+        return "home";
+    }
+
+    @PostMapping("/buscarModelo")
+    public String obtenerModelo(@RequestParam("idModelo") int idModelo, Model model){
+        model.addAttribute("mensaje","Mensaje desde model");
+        
+        Modelo modelo = serviceDependencia.buscarPorId(idModelo);
+        model.addAttribute("modelo", modelo);
+
+        List<Modelo> lista = serviceDependencia.obtenerModelos();
+        model.addAttribute("modelos", lista);
         return "home";
     }
 }
