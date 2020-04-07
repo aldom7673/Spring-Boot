9126171c4be060126eba12fe9cd764ceadcb12e9
= <aldom7673@gmail.com>
2020-04-06 19:23:57 -0500

    thymeleaf

--- demoartifact/src/main/java/comgroupid/demoartifact/Controlador/HomeController.java
+++ demoartifact/src/main/java/comgroupid/demoartifact/Controlador/HomeController.java
@@ -1,13 +1,26 @@
 package comgroupid.demoartifact.Controlador;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String Inicio(){
+    public String Inicio(Model model){
+        List<String> lista = new LinkedList<String>();
+        lista.add("Valor 1");
+        lista.add("Valor 2");
+        lista.add("Valor 3");
+        lista.add("Valor 4");
+        lista.add("Valor 5");
+        lista.add("Valor 6");
+        model.addAttribute("mensaje","Mensaje desde model");
+        model.addAttribute("lista", lista);
         return "home";
     }
 }
