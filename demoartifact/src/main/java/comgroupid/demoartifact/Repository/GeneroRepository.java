2af9d2a6be72cdafcf6babd63213af71a5e30c3b
= <aldom7673@gmail.com>
2020-04-14 16:05:22 -0500

    Repositorio de la clase

---
+++ demoartifact/src/main/java/comgroupid/demoartifact/Repository/GeneroRepository.java
@@ -0,0 +1,9 @@
+package comgroupid.demoartifact.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import comgroupid.demoartifact.Modelo.Genero;
+
+public interface GeneroRepository extends JpaRepository<Genero,Integer>{
+
+}
