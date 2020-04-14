221ee0669e10f55053238650a0335f1c8875a256
= <aldom7673@gmail.com>
2020-04-14 16:04:49 -0500

    Repository Usuario

--- demoartifact/src/main/java/comgroupid/demoartifact/DemoApplication.java
+++ demoartifact/src/main/java/comgroupid/demoartifact/DemoApplication.java
@@ -1,5 +1,6 @@
 package comgroupid.demoartifact;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,14 +8,22 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import comgroupid.demoartifact.Modelo.Genero;
 import comgroupid.demoartifact.Modelo.TipoUsuario;
+import comgroupid.demoartifact.Modelo.Usuario;
+import comgroupid.demoartifact.Repository.GeneroRepository;
 import comgroupid.demoartifact.Repository.TipoUsuarioRepository;
+import comgroupid.demoartifact.Repository.UsuarioRepository;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 
 	@Autowired
 	private TipoUsuarioRepository repositoryTipoUsuario;
+	@Autowired
+	private UsuarioRepository repositoryUsuario;
+	@Autowired
+	private GeneroRepository repositoryGenero;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
@@ -26,7 +35,24 @@ public class DemoApplication implements CommandLineRunner {
 		//Guardar();
 		//BuscarPorId();
 		//Modificar();
-		EliminarPorId();
+		//EliminarPorId();
+		ObtenerUsuarios();
+		CrearUsuario();
+		ObtenerUsuarios();
+	}
+	
+	private void CrearUsuario(){
+		Optional<TipoUsuario> tipoUsuario = repositoryTipoUsuario.findById(3);
+		Optional<Genero> genero = repositoryGenero.findById(1);
+		Usuario usuario = new Usuario("Aldo2", "Mendoza2", "Morales2", "correo", "contra", tipoUsuario.get(), genero.get(),true);
+		repositoryUsuario.save(usuario);
+	}
+
+	private void ObtenerUsuarios(){
+		List<Usuario> usuarios = repositoryUsuario.findAll();
+		for(Usuario u : usuarios){
+			System.out.println(u);
+		}
 	}
 
 	private void Guardar(){
