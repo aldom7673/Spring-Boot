e9bc133d8dcc7e9ef5fc99697f4e65f6fd420d62
= <aldom7673@gmail.com>
2020-04-14 02:46:32 -0500

    Repositorio del modelo

---
+++ demoartifact/src/main/java/comgroupid/demoartifact/Repository/TipoUsuarioRepository.java
@@ -0,0 +1,9 @@
+package comgroupid.demoartifact.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import comgroupid.demoartifact.Modelo.TipoUsuario;
+
+public interface TipoUsuarioRepository extends CrudRepository<TipoUsuario, Integer>{
+
+}
