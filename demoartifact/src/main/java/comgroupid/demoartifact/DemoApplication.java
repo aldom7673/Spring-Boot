2df83cb000887992b0c40c02abd57aea54e2048e
= <aldom7673@gmail.com>
2020-04-14 02:47:06 -0500

    CRUD de clase TipoUsuario

--- demoartifact/src/main/java/comgroupid/demoartifact/DemoApplication.java
+++ demoartifact/src/main/java/comgroupid/demoartifact/DemoApplication.java
@@ -1,13 +1,62 @@
 package comgroupid.demoartifact;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DemoApplication {
+import comgroupid.demoartifact.Modelo.TipoUsuario;
+import comgroupid.demoartifact.Repository.TipoUsuarioRepository;
 
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private TipoUsuarioRepository repositoryTipoUsuario;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Ejemplos JPA");
+		System.out.println(repositoryTipoUsuario);
+		//Guardar();
+		//BuscarPorId();
+		//Modificar();
+		EliminarPorId();
+	}
+
+	private void Guardar(){
+		TipoUsuario tipoUsuario = new TipoUsuario("Segundo tipo usuario", "Descripcion 2", true);
+		repositoryTipoUsuario.save(tipoUsuario);
+		System.out.println(tipoUsuario);
+	}
+	
+	private void BuscarPorId(){
+		Optional<TipoUsuario> tipoUsuario = repositoryTipoUsuario.findById(3);
+		if( tipoUsuario.isPresent() )
+			System.out.println(tipoUsuario.get());
+		else
+			System.out.println("Entidad no encontrada");
+	}
+
+	private void Modificar(){
+		Optional<TipoUsuario> optional = repositoryTipoUsuario.findById(3);
+		if( optional.isPresent() ){
+			TipoUsuario tipoUsuario = optional.get();
+			tipoUsuario.setEstado(false);
+			tipoUsuario.setDescripcion("descripcion modificada");
+			tipoUsuario.setTipoUsuario("tipo de usuario modificado");
+			repositoryTipoUsuario.save(tipoUsuario);
+		}
+		else
+			System.out.println("Entidad no encontrada");
+	}
+
+	private void EliminarPorId(){
+		repositoryTipoUsuario.deleteById(2);		
+	}
 }
