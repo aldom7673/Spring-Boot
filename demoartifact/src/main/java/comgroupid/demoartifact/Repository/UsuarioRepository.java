7d2e1587d4717215b2cdd383bb3d5a306d218d48
= <aldom7673@gmail.com>
2020-04-24 01:12:29 -0500

    Query Method

--- demoartifact/src/main/java/comgroupid/demoartifact/Repository/UsuarioRepository.java
+++ demoartifact/src/main/java/comgroupid/demoartifact/Repository/UsuarioRepository.java
@@ -1,9 +1,11 @@
 package comgroupid.demoartifact.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import comgroupid.demoartifact.Modelo.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario,Integer>{
-
+    List<Usuario> findByEstado(boolean estado);
 }
